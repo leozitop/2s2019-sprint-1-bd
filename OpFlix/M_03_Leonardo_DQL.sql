4c8fca32c4491a7f65454ed639d129b26e169bcb
Leonardo Do Nascimento Pereira <45684686800@info.local>
2019-08-14 11:08:16 -0300

    Gufos

--- OpFlix/M_03_Leonardo_DQL.sql
+++ OpFlix/M_03_Leonardo_DQL.sql
@@ -16,6 +16,7 @@ join Lancamentos L
 ON UL.IdLancamento = L.IdLancamento;
 
 Select LEN('La Casa de Papel');
+--quantidade de caracteres de um lançamento--
 
 Create Procedure Linhas
 @QtdLinhas varchar(200)
@@ -26,13 +27,41 @@ where IdLancamento >= 1
 
 Execute Linhas '1';
 Drop Procedure Linhas;
+--Quantidade de lançamentos--
 
-Create Procedure FilmeCategoria
-@FilmeCategoria varchar(200)
+SELECT DATEDIFF(day, getDate(), '2019-10-03');
+--Diferença em dias até a data de lançamento do filme "Coringa"--
+
+Create Procedure BuscarAcao
+@CampoBusca varchar(200)
 As
-select count(*)
+select count(IdCategoria)
 from Lancamentos
-where IdCategoria = 1
+where IdCategoria = @CampoBusca
 
-Execute FilmeCategoria '1';
-Drop Procedure FilmeCategoria;
+Execute BuscarAcao '1';
+Drop Procedure BuscarAcao;
+--buscar por filmes de Ação--
+
+Create Procedure Buscar
+@CampoBusca varchar(200)
+As
+select count(IdCategoria)
+from Lancamentos
+where IdCategoria = @CampoBusca
+
+Execute Buscar '4';
+Drop Procedure Buscar;
+--buscar filmes pelo id da categoria--
+
+Create Procedure CategoriaPlataforma
+@QtdLancamentos varchar(200)
+As
+select count(IdPlataforma)
+from Lancamentos
+where Idplataforma = @QtdLancamentos
+
+Execute CategoriaPlataforma '4';
+Drop Procedure CategoriaPlataforma;
+
+select * from Lancamentos;
